a41009ebcfab7270d4cb57e0779e6f0f5b291fcf
Aviv Salem <avivsalem@gmail.com>
2021-12-01 15:36:50 +0200

    added ThreadLocalMember descriptor

--- baseservice/utils.py
+++ baseservice/utils.py
@@ -1,4 +1,5 @@
-from typing import Collection, TypeVar,List, Iterator,Generic,Callable
+import threading
+from typing import Collection, TypeVar, List, Iterator, Generic, Callable, Optional
 from itertools import cycle, islice
 
 
@@ -7,39 +8,43 @@ class KwargsException(Exception):
         super().__init__(*args)
         self.kwargs = kwargs
 
+
 TItemType = TypeVar('TItemType')
 
+
 class StatefulListIterator(Collection[TItemType]):
-    def __init__(self, items:List[TItemType]) -> None:
+    def __init__(self, items: List[TItemType]) -> None:
         self._items = items
         self._item_count = len(items)
         self._item_cycle = cycle(self._items)
 
     def __contains__(self, __x: object) -> bool:
         return self._items.__contains__(__x)
-    
+
     def __len__(self) -> int:
         return self._item_count
-    
+
     def __iter__(self) -> Iterator[TItemType]:
         return islice(self._item_cycle, self._item_count)
 
 
 TEventType = TypeVar('TEventType')
+
+
 class Event(Generic[TEventType]):
     def __init__(self) -> None:
         self._handlers: List[Callable[[TEventType], None]] = []
 
     def register_handler(self, handler: Callable[[TEventType], None]) -> None:
-        self._handlers.append(handler)  
+        self._handlers.append(handler)
 
     def unregister_handler(self, handler: Callable[[TEventType], None]) -> bool:
         if handler in self._handlers:
             self._handlers.remove(handler)
             return True
         return False
-    
-    def fire(self, event: TEventType, continue_after_failure:bool=True) -> bool:
+
+    def fire(self, event: TEventType, continue_after_failure: bool = True) -> bool:
         ret_val = True
         for handler in self._handlers:
             try:
@@ -49,6 +54,53 @@ class Event(Generic[TEventType]):
                 if not continue_after_failure:
                     raise
         return ret_val
-        
 
-        
+
+TValueType = TypeVar('TValueType')
+
+
+class ThreadLocalValue(threading.local, Generic[TValueType]):
+    """
+    this class holds a single, thread-local value
+
+    x = ThreadLocalValue(5)
+    x.value # 5
+    x.value = 7 # changes only for this thread
+    """
+    def __init__(self, init_value: Optional[TValueType] = None):
+        self.value = init_value
+
+
+class ThreadLocalMember(Generic[TValueType]):
+    """
+    this is a descriptor, for making thread local memebers for class:
+
+    class MyClass:
+        x = ThreadLocalMember(5)
+
+    a = MyClass():
+    a.x # 5
+    a.x = 7 # changes only for this thread
+    """
+    def __init__(self, init_value: Optional[TValueType] = None):
+        self._init_value = init_value
+
+    def __set_name__(self, owner, name):
+        self._private_name = f'_thread_local_{name}'
+
+    def _get_local_value(self, instance) -> ThreadLocalValue:
+        if hasattr(instance, self._private_name):
+            lv = getattr(instance, self._private_name)
+        else:
+            lv = ThreadLocalValue(self._init_value)
+            setattr(instance, self._private_name, lv)
+
+        return lv
+
+    def __get__(self, instance, owner) -> Optional[TValueType]:
+        lv = self._get_local_value(instance)
+        return lv.value
+
+    def __set__(self, instance, value):
+        lv = self._get_local_value(instance)
+        lv.value = value
---
+++ tests/utils_test.py
@@ -0,0 +1,31 @@
+from baseservice.utils import ThreadLocalMember
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor()
+
+def test_thread_local_member():
+    class MyTestClass:
+        prop1: ThreadLocalMember[int] = ThreadLocalMember(5)
+        prop2: ThreadLocalMember[str] = ThreadLocalMember()
+
+    a = MyTestClass()
+    assert a.prop1 == 5
+    assert a.prop2 is None
+
+    a.prop1 = 4
+    a.prop2 = "Bla"
+
+    b = MyTestClass()
+    assert b.prop1 == 5
+    assert b.prop2 is None
+
+    def func():
+        b.prop2 = "this is thread"
+        return a.prop1, a.prop2, b.prop1, b.prop2
+
+    fut = executor.submit(func)
+    assert fut.result() == (5, None, 5, "this is thread")
+    assert b.prop2 is None
+
+
+
+
