74f851f4f887fd3a99e3d93eddeb2450ee403376
noamisraeli <43722678+noamisraeli@users.noreply.github.com>
2021-12-05 19:58:15 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='BaseService',
+      version='0.1a',
+      author='Aviv Salem',
+      author_email='avivsalem@gmail.com',
+      url='https://github.com/Avivsalem/BaseService',
+      packages=['baseservice'],
+     )
