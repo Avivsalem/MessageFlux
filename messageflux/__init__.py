f51de5e0875f3c8248ac1f58c9b7f700e7401c67
avivs <avivsalem@gmail.com>
2023-04-15 17:26:13 +0300

    added plugins to mkdocs

--- messageflux/__init__.py
+++ messageflux/__init__.py
@@ -1,5 +1,10 @@
 from messageflux.iodevices.base import InputDevice, ReadResult
+
 from .message_handling_service import (BatchMessageHandlingService,
                                        MessageHandlerBase,
                                        MessageHandlingService,
                                        BatchMessageHandlerBase)
+
+from .pipeline_service import (PipelineService,
+                               PipelineResult,
+                               PipelineHandlerBase)
