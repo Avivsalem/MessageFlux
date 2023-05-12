ec4c428bcd6c50620e3b4f0d8e342da72b9874b9
avivs <avivsalem@gmail.com>
2023-05-12 08:47:43 +0300

    3.7 doesn't allow positional only vars

--- tests/fastmessage_handler_test.py
+++ tests/fastmessage_handler_test.py
@@ -144,14 +144,17 @@ def test_default_value_on_special_param():
             pass
 
 
-def test_positional_and_var_args():
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="fails on 3.7")
+def test_positional_arg():
     fm: FastMessage = FastMessage()
-    if sys.version_info.minor >= 8:
-        with pytest.raises(NotAllowedParamKindException):
-            @fm.map(input_device='input1')
-            def do_something(x: int, /, z: int):  # noqa: E999
-                pass
+    with pytest.raises(NotAllowedParamKindException):
+        @fm.map(input_device='input1')
+        def do_something(x: int, /, z: int):  # noqa: E999
+            pass
 
+
+def test_var_args():
+    fm: FastMessage = FastMessage()
     with pytest.raises(NotAllowedParamKindException):
         @fm.map(input_device='input1')
         def do_something1(x: int, *args):
